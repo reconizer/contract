3076e9f1967516118754233088663bec3178a934
Nick Kutryj <nick@tokagen.com>
2019-02-25 10:49:09 +0100

    Initial commit

---
+++ lib/contract.ex
@@ -0,0 +1,174 @@
+defmodule Contract do
+  @type validate_params_t :: %{required(atom()) => any()}
+  @type error :: {:error, any()}
+  @type validate_params :: validate_params_t | {:ok, validate_params_t} | error
+  @type validate_result :: {:ok, %{required(atom()) => any()}} | error
+
+  def plug({:ok, params}, plugs), do: plug(params, plugs)
+  def plug({:error, _} = error, _), do: error
+
+  def plug(params, plugs) do
+    plugs
+    |> Enum.reduce({:ok, params}, fn
+      {plug_key, plug_fun}, {:ok, params} when is_function(plug_fun, 1) ->
+        params
+        |> Map.get(plug_key, :undefined)
+        |> case do
+          :undefined ->
+            {:ok, params}
+
+          value ->
+            plug_fun.(value)
+            |> case do
+              {:ok, value} -> {:ok, params |> Map.put(plug_key, value)}
+              other -> other
+            end
+        end
+
+      {plug_key, plug_fun}, {:ok, params} when is_function(plug_fun, 2) ->
+        params
+        |> Map.get(plug_key, :undefined)
+        |> case do
+          :undefined ->
+            {:ok, params}
+
+          value ->
+            plug_fun.(value, params)
+            |> case do
+              {:ok, value} -> {:ok, params |> Map.put(plug_key, value)}
+              other -> other
+            end
+        end
+
+      {_plug_key, _plug_fun}, {:error, _} = error ->
+        error
+    end)
+  end
+
+  def default({:ok, params}, defaults) do
+    default(params, defaults)
+  end
+
+  def default({:error, _} = error, _) do
+    error
+  end
+
+  def default(params, defaults) do
+    defaults
+    |> Enum.reduce(params, fn {key, value}, params ->
+      params
+      |> Map.get(key)
+      |> case do
+        nil -> params |> Map.put(key, value)
+        _ -> params
+      end
+    end)
+  end
+
+  def cast({:ok, params}, types) do
+    cast(params, types)
+  end
+
+  def cast({:error, _} = error, _) do
+    error
+  end
+
+  def cast(params, types) do
+    type_keys = types |> Map.keys() |> Enum.map(&"#{&1}")
+
+    initial =
+      params
+      |> Enum.map(fn
+        {key, _value} when is_atom(key) ->
+          {key, nil}
+
+        {key, _value} when is_bitstring(key) ->
+          (key in type_keys)
+          |> case do
+            true ->
+              {key |> String.to_existing_atom(), nil}
+
+            false ->
+              nil
+          end
+      end)
+      |> Enum.filter(& &1)
+      |> Enum.into(%{})
+
+    {initial, types}
+    |> Ecto.Changeset.cast(params, Map.keys(types))
+    |> resolve_changeset
+  end
+
+  @spec validate(validate_params, Map.t()) :: validate_result
+  def validate({:ok, params}, validations), do: validate(params, validations)
+  def validate({:error, _} = error, _), do: error
+
+  def validate(params, validations) do
+    param_keys = for key <- params |> Map.keys(), do: {key, :any}, into: %{}
+    validation_keys = for key <- validations |> Map.keys(), do: {key, :any}, into: %{}
+
+    keys = Map.merge(param_keys, validation_keys)
+
+    initial =
+      params
+      |> Enum.map(fn
+        {key, _value} -> {key, nil}
+      end)
+      |> Enum.into(%{})
+
+    changeset =
+      {initial, keys}
+      |> Ecto.Changeset.cast(params, keys |> Map.keys())
+
+    validations
+    |> Enum.reduce(changeset, &validate_changeset/2)
+    |> resolve_changeset
+  end
+
+  defp resolve_changeset(changeset) do
+    changeset
+    |> case do
+      %{valid?: true} = changeset ->
+        {:ok, changeset |> Ecto.Changeset.apply_changes()}
+
+      errors ->
+        {:error, errors}
+    end
+  end
+
+  defp validate_changeset({key, key_validations}, changeset) when is_list(key_validations) do
+    key_validations
+    |> Enum.reduce(changeset, fn validation, changeset ->
+      changeset
+      |> do_validate(key, validation)
+    end)
+  end
+
+  defp validate_changeset({key, key_validation}, changeset) do
+    changeset
+    |> do_validate(key, key_validation)
+  end
+
+  defp do_validate(changeset, key, :required) do
+    changeset
+    |> Ecto.Changeset.validate_required(key)
+  end
+
+  defp do_validate(changeset, key, fun) when is_function(fun) do
+    changeset
+    |> Ecto.Changeset.validate_change(key, fn _, value ->
+      fun.(value)
+      |> case do
+        true -> []
+        false -> [{key, "is invalid"}]
+        nil -> []
+        error -> [{key, error}]
+      end
+    end)
+  end
+
+  defp do_validate(changeset, _, _) do
+    changeset
+  end
+end
---
+++ test/contract_test.exs
@@ -0,0 +1,133 @@
+defmodule ContractTest do
+  use ExUnit.Case
+  doctest Contract
+
+  test "cast/2" do
+    params = %{"foo" => "bar", "bar" => 2, "test" => [1, 2, 3, 4]}
+
+    assert {:ok, _} =
+             params |> Contract.cast(%{foo: :string, bar: :integer, test: {:array, :integer}})
+
+    assert {:error, _} = params |> Contract.cast(%{bar: :string})
+  end
+
+  test "plug/2" do
+    params = %{some: "parameter", other: 123, test: [1, 2, 3, 4]}
+
+    result =
+      params
+      |> Contract.plug(
+        some: fn value ->
+          {:ok, "#{value} - test"}
+        end,
+        other: fn value ->
+          {:ok, value + 1111}
+        end,
+        test: fn value ->
+          {:ok, value ++ [5, 6, 7, 8]}
+        end
+      )
+
+    assert {:ok, %{some: "parameter - test", other: 1234, test: [1, 2, 3, 4, 5, 6, 7, 8]}} ==
+             result
+  end
+
+  test "plug/2 accepting 2 arguments for function" do
+    params = %{some: "parameter", other: 123}
+
+    result =
+      params
+      |> Contract.plug(
+        some: fn value, params ->
+          {:ok, "#{value} - #{params.other}"}
+        end
+      )
+
+    assert {:ok, %{some: "parameter - 123", other: 123}} == result
+  end
+
+  test "plug/2 with failed plug" do
+    params = %{some: "parameter", other: 123, test: [1, 2, 3, 4]}
+
+    result =
+      params
+      |> Contract.plug(
+        some: fn value ->
+          {:error, :is_invalid}
+        end,
+        other: fn value ->
+          {:ok, value + 1111}
+        end,
+        test: fn value ->
+          {:ok, :is_also_invalid}
+        end
+      )
+
+    assert {:error, :is_invalid} == result
+  end
+
+  test "cast/2 with unknown atom string" do
+    params = %{"some_non_existent_atom" => "bar", "bar" => 2, "test" => [1, 2, 3, 4]}
+
+    assert {:ok, %{bar: _, test: _}} =
+             params |> Contract.cast(%{foo: :string, bar: :integer, test: {:array, :integer}})
+
+    assert {:error, _} = params |> Contract.cast(%{bar: :string})
+  end
+
+  test "cast/2 with atoms" do
+    params = %{foo: "bar", test: [1, 2, 3]}
+
+    assert {:ok, %{foo: _, test: _}} =
+             params |> Contract.cast(%{foo: :string, test: {:array, :integer}})
+
+    assert {:error, _} = params |> Contract.cast(%{test: :string})
+  end
+
+  test "validate/2 with atom keys" do
+    params = %{foo: 2}
+
+    assert {:ok, _} = params |> Contract.validate(%{foo: :required})
+    assert {:error, _} = params |> Contract.validate(%{bar: :required})
+
+    assert {:ok, _} =
+             params
+             |> Contract.validate(%{
+               foo: fn value ->
+                 (value == 2)
+                 |> case do
+                   true -> nil
+                   _ -> "invalid value"
+                 end
+               end
+             })
+
+    assert {:error, _} =
+             params
+             |> Contract.validate(%{
+               foo: fn value ->
+                 (value > 2)
+                 |> case do
+                   true -> nil
+                   _ -> "must be greater than 2"
+                 end
+               end
+             })
+
+    assert {:error, _} =
+             params
+             |> Contract.validate(%{
+               foo: fn value ->
+                 value > 2
+               end
+             })
+
+    assert {:ok, _} =
+             params
+             |> Contract.validate(%{
+               foo: fn value ->
+                 value >= 2
+               end
+             })
+  end
+end
